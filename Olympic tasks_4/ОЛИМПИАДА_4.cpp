0fdf6723553877a6e4ac60fbbfbf2b5e14c36f9a
Mornival17 <rthbeberf@gmail.com>
2025-07-20 11:35:19 +0000

    Olympic tasks_4

---
+++ Olympic tasks_4/ОЛИМПИАДА_4.cpp
@@ -0,0 +1,229 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+//int zadenie_4(int n) {
+//	int summ = 0;
+//	if (n != 0) {
+//		int g = n % 10;
+//		summ+=g;
+//		summ += zadenie_4(n / 10);
+//			}
+//	return summ;
+//}
+
+int main()
+{
+
+	// Задание 1
+
+	//int n, up=0, on=0;
+	//cin >>  n;
+
+	//up += n / 4;
+	//on += n % 4;
+
+	//if (on > 0) {
+	//	up += 1;
+	//	on -= on;
+	//}
+
+	//cout << up << " " << on;
+
+
+	//int n;
+	//std::cin >> n;
+
+	//int min = n / 5;
+
+	//int oct = n % 5;
+
+	//int day = oct > 0 ? oct : 0;
+
+	//cout << min << " " << day;
+
+	// Задание 1
+
+	// Задание 2
+
+	//int N, k, kor, pol;
+	//cin >> N >> k;
+	//pol = k / N;
+	//kor = k % N;
+	//cout << pol << " " << kor;
+
+	//int sum=0, n;
+	//cin >> n;
+	//sum += n % 10;
+	//n = n / 10;
+	//sum += n % 10;
+	//n = n / 10;
+	//sum += n % 10;
+	//cout << sum;
+
+	// Задание 2
+
+	// Задание 3
+
+	//int a, b, c;
+	//cin >> a >> b >> c;
+	//c = (c - a);
+	//int buf = c / b;
+	//cout << buf;
+
+	//int n, b; 
+	//cin >> n;
+	//if (n < 10) {
+	//	cout << 0;
+	//}
+	//else {
+	//	b = n / 10;
+	//	b = b % 10;
+	//	cout << b;
+	//}
+
+	// Задание 3
+
+	// Задание 4
+
+
+	//int chas, min, n;
+	//cin >> n;
+	//chas = n / 60;
+	//min = n % 60;
+	//cout << chas << " " << min;
+
+	//int n,sum=0; 
+	//cin >> n;
+	//sum += zadenie_4(n);
+	//cout << sum;
+
+	// Задание 4
+
+	// Задание 5
+
+	//int d, R, L, n;
+	//cin >> d >> R >> n;
+	//L = 2 * R * n;
+	//cout << L;
+
+
+	//float rub, kop, a, b, n;
+	//cin >> a >> b >> n;
+	//rub = (a + b*0.01) * n;
+	//kop = rub * 100;
+	//cout << rub<<endl;
+	//cout << kop << endl;
+
+	// Задание 5
+
+
+	// Задание 6
+
+	//int n, k;
+	//cin >> n >> k;
+	//int apples_per_student = k / n;
+
+	//int remainder = k % n;
+
+	//int students_with_less_apples = n - remainder;
+
+	//std::cout << students_with_less_apples << std::endl;
+
+	//int n, m;
+	//cin >> n >> m;
+	//cout << endl;
+
+	//float dnei;
+	//dnei = m / n;
+	//cout << dnei << endl;
+
+	// Задание 6
+
+	// Задание 7
+
+	//int a, b, c;
+	//cin >> a >> b >> c;
+
+	//int sum = a + b + c;
+
+	//if ((a+b+c) % 2 == 0) {
+	//	sum = sum / 2;
+	//}
+	//else {
+	//	sum = sum + 1;
+	//	sum = sum / 2;
+	//}
+
+	//cout << sum << endl;
+
+	//int k, n;
+	//cin >> k;
+	//cout << endl;
+	//cin >> n;
+	//int ctr = (n - 1) / k + 1;
+	//int line = (n - 1) % k + 1;
+	//cout << ctr << endl;
+	//cout << line << endl;
+
+
+	// Задание 7
+	
+	// Задание 8
+
+	//int a, n, m;
+	//cin >> a;
+	//if (a % 2 == 0) {
+	//	cout << a + 2;
+	//}
+	//else {
+	//	cout << a + 1;
+	//}
+
+	//cout << endl;
+
+	//cin >> n >> m;
+	//int sum;
+	//
+	//if (m % 2 == 0) {
+	//	sum = (m / 2) * n;
+	//}
+	//else if (m % 2 != 0) {
+	//	sum = (n>m) ? (n * m)/2 : ((n * m) / 2) + 1;
+	//}
+
+	//cout << sum;
+
+
+	// Задание 8
+ 
+	// Задание 9
+
+	//int a, b = 0;
+	//cin >> a;
+
+	//string str = to_string(a);
+	//str = string(4 - str.length(), '0') + str;
+	//if (str == string(str.rbegin(), str.rend())) {
+	//	cout << 1 << endl;
+	//}
+	//else {
+	//	cout << 0 << endl;
+	//}
+
+	//cout << endl;
+
+	//int aa, bb, min=0, max=0;
+	//cin >> aa>>bb;
+	//max = (aa + bb + abs(aa - bb)) / 2;
+	//min = (aa + bb - abs(aa - bb)) / 2;
+	//cout << "big " << max<<endl;
+	//cout << "smol " << min << endl;
+
+	// Задание 9
+
+
+	return 0;
+}
+
