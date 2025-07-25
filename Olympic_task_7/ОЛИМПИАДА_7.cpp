276b826d78f87f5c2a7c5aaa530f03e83a780667
Mornival17 <rthbeberf@gmail.com>
2025-07-25 10:57:45 +0000

    Olympic_tasc_7

---
+++ Olympic_task_7/ОЛИМПИАДА_7.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+	// Задание 3
+
+	string str;
+
+	int n = 3;
+	int a = 1, b = 4, c = 11;
+
+	int maxS = max(a, b);
+	int maxA = max(maxS, c);
+
+	for (int i = 2; i < maxA; i++) {
+		bool flag = true;
+		for (int j = 2; j * j <= i; j++) {
+			if (i % j == 0) {
+				flag = false;
+				break;
+			}
+		}
+		if (flag) {
+			str += to_string(i);
+		}
+	}
+
+
+	cout << str[a] << endl;
+	cout << str[b] << endl;
+	cout << str[c] << endl;
+
+	cout << str << endl;
+
+	cout << str;
+
+	// Задание 3
+
+	return 0;
+}
